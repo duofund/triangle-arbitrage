a699dc8b8fc326a959b2089b1979766d53d8c2b8
georgezouq <zousongqi@foxmail.com>
2018-07-20 12:29:19 +0800

    Change api part to ccxt for multi exchange

--- index.js
+++ index.js
@@ -1,76 +1,78 @@
 // load logger library
-const logger = require('./lib/LoggerCore');
+const logger = require('./lib/LoggerCore')
+const ccxt = require('ccxt')
+const binanceApi = require('binance')
 
-var env = require('node-env-file');
-env(__dirname + '/.keys');
-env(__dirname + '/conf.ini');
+const env = require('node-env-file')
+env(__dirname + '/.keys')
+env(__dirname + '/conf.ini')
+
+const exchangeAPI = {}
+const exchangeName = process.env.activeExchange
 
 if (!process.env.binance_key || !process.env.binance_secret) {
-  throw 'Error: Specify your binance API settings in a file called ".keys". The .keys-template can be used as a template for how the .keys file should look.';
+    throw 'Error: Specify your binance API settings in a file called ".keys". The .keys-template can be used as a template for how the .keys file should look.'
 }
 
-logger.info('\n\n\n----- Bot Starting : -----\n\n\n');
+logger.info('\n\n\n----- Bot Starting : -----\n\n\n')
+logger.info(`--- Loading Exchange API `)
+logger.info('--- \tActive Exchange:' + process.env.activeExchange)
 
-var exchangeAPI = {};
 
-logger.info('--- Loading Exchange API');
-
-// make exchange module dynamic later
-if (process.env.activeExchange == 'binance'){
-  logger.info('--- \tActive Exchange:' + process.env.activeExchange);
-  // activePairs = process.env.binancePairs;
-  
-  const api = require('binance');
-  exchangeAPI = new api.BinanceRest({
-    key: process.env.binance_key,
-    secret: process.env.binance_secret,
+const exchangeAPI = getattr(ccxt, exchangeName)({
+    'verbose': true,
+    'apiKey': process.env[`${exchangeName}_key`],
+    'secret': process.env[`${exchangeName}_secret`],
     timeout: parseInt(process.env.restTimeout), // Optional, defaults to 15000, is the request time out in milliseconds
     recvWindow: parseInt(process.env.restRecvWindow), // Optional, defaults to 5000, increase if you're getting timestamp errors
     disableBeautification: process.env.restBeautify != 'true'
-  });
-  exchangeAPI.WS = new api.BinanceWS();
+})
+
+if (exchangeName == 'binance') {
+    exchangeAPI.WS = new binanceApi.BinanceWS()
 }
 
-var botOptions = {
+const botOptions = {
     UI: {
-      title: 'Top Potential Arbitrage Triplets, via: ' + process.env.binanceColumns
+        title: 'Top Potential Arbitrage Triplets, via: ' + process.env.binanceColumns
     },
     arbitrage: {
-      paths: process.env.binanceColumns.split(','),
-      start: process.env.binanceStartingPoint
+        paths: process.env.binanceColumns.split(','),
+        start: process.env.binanceStartingPoint
     },
     storage: {
-      logHistory: false
+        logHistory: false
     },
     trading: {
-      paperOnly: false,
-      // only candidates with over x% gain potential are queued for trading
-      // minQueuePercentageThreshold: 0.3,
-      minQueuePercentageThreshold: 0.19,
-      // how many times we need to see the same opportunity before deciding to act on it
-      minHitsThreshold: 1,
-      mainCoinQuantityLimit: process.env.mainCoinQuantityLimit,
-      percentageOfFee: 0.05,
-      percentageOfBestQuantity: 0.8,
-      tradeTime: 2
+        paperOnly: false,
+        // only candidates with over x% gain potential are queued for trading
+        // minQueuePercentageThreshold: 0.3,
+        minQueuePercentageThreshold: 0.19,
+        // how many times we need to see the same opportunity before deciding to act on it
+        minHitsThreshold: 1,
+        mainCoinQuantityLimit: process.env.mainCoinQuantityLimit,
+        percentageOfFee: 0.05,
+        percentageOfBestQuantity: 0.8,
+        tradeTime: 2
     }
-  },
-  ctrl = {
+}
+
+const ctrl = {
     options: botOptions,
     storage: {
-      trading: {
-      // queued triplets
+        trading: {
+        // queued triplets
         queue: [],
         // actively trading triplets
         active: []
-      },
-      candidates: [],
-      streams: [],
-      pairRanks: []
+        },
+        candidates: [],
+        streams: [],
+        pairRanks: []
     },
     logger: logger,
     exchange: exchangeAPI
-  };
+}
 
 function getMiniQuantity () {
     ctrl.exchange.exchangeInfo().then(data => {
@@ -81,7 +83,6 @@ function getMiniQuantity () {
         })
 
         ctrl.miniQuantity = miniQuantity
-        console.log('[Mini Quantity]: ', miniQuantity, 'data:', data)
         ctrl.exchange.startTimeSync(1000)
     }).catch(e => {
       console.log('[Mini Quantity] Error', e)
@@ -93,21 +94,21 @@ getMiniQuantity()
 
 // load DBCore, then start streams once DB is up and connected
 require('./lib/DBCore')(logger, (err, db)=>{
-  if (process.env.useMongo == 'true'){
-    ctrl.storage.db = db;
-    ctrl.options.storage.logHistory = true;
-  }
-  
-  if (err){
-    ctrl.logger.error('MongoDB connection unavailable, history logging disabled: ' + err);
-    ctrl.options.storage.logHistory = false;
-  }
-  
-  ctrl.UI       = require('./lib/UI')(ctrl.options),
-  ctrl.events   = require('./lib/EventsCore')(ctrl);
+    if (process.env.useMongo == 'true'){
+        ctrl.storage.db = db
+        ctrl.options.storage.logHistory = true
+    }
 
-  // We're ready to start. Load up the webhook streams and start making it rain.
-  require('./lib/BotCore')(ctrl);
-  
-  ctrl.logger.info('----- Bot Startup Finished -----');
-});
+    if (err){
+        ctrl.logger.error('MongoDB connection unavailable, history logging disabled: ' + err)
+        ctrl.options.storage.logHistory = false
+    }
+
+    ctrl.UI       = require('./lib/UI')(ctrl.options),
+    ctrl.events   = require('./lib/EventsCore')(ctrl)
+
+    // We're ready to start. Load up the webhook streams and start making it rain.
+    require('./lib/BotCore')(ctrl)
+
+    ctrl.logger.info('----- Bot Startup Finished -----')
+})
